0076839e939706a69fac1afa01cbb6b46729d77a
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-02-25 12:50:56 +0530

    Added the logic to generate next generation alive and daid.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Board } from "./Board";
+import { ROWS, COLOMS } from "../constants";
 const Header = styled.header`
   margin: 0;
   padding: 0;
@@ -26,10 +27,20 @@ const GameStatus = styled.button`
 
 function App() {
   const [gameStatus, setGameStatus] = useState(false);
+  const newBoardStatus = (cellStatus = () => Math.random() < 0.25) => {
+    const grid = [];
+    for (let r = 0; r < ROWS; r++) {
+      grid[r] = [];
+      for (let c = 0; c < COLOMS; c++) {
+        grid[r][c] = cellStatus();
+      }
+    }
+    return grid;
+  };
   return (
     <div>
       <Header>Game of life</Header>
-      <Board gameStatus={gameStatus} />
+      <Board gameStatus={gameStatus} boardStatus={newBoardStatus()} />
       <GameStatus
         onClick={() => setGameStatus(!gameStatus)}
         gameStatus={gameStatus}
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Square } from "./Square";
 import { ROWS, COLOMS } from "../constants";
@@ -17,11 +17,58 @@ const BoardStyle = styled.main`
   box-sizing: border-box;
 `;
 
-export const Board = ({ gameStatus }) => {
+export const Board = ({ gameStatus, boardStatus }) => {
   const isAlive = () => Math.random() > 0.7;
+  const [tr, setTr] = useState([]);
+  console.log(gameStatus, "gameStatus");
 
-  const renderRow = () => {
-    const tr = [];
+  const handleStart = () => {
+    const newBoardStatus = JSON.parse(JSON.stringify(boardStatus));
+    // setInterval(() => {
+    for (let r = 0; r < ROWS; r++) {
+      for (let c = 0; c < COLOMS; c++) {
+        let trueNeighbors = 0;
+        const neighbors = [
+          [-1, -1],
+          [-1, 0],
+          [-1, 1],
+          [0, 1],
+          [1, 1],
+          [1, 0],
+          [1, -1],
+          [0, -1]
+        ];
+        neighbors.forEach(neighbor => {
+          const x = r + neighbor[0];
+          const y = c + neighbor[1];
+          const isNeighborOnBoard = x >= 0 && x < ROWS && y >= 0 && y < COLOMS;
+          /* No need to count more than 4 alive neighbors */
+          if (trueNeighbors < 4 && isNeighborOnBoard && boardStatus[x][y]) {
+            trueNeighbors++;
+          }
+        });
+        if (!boardStatus[r][c]) {
+          if (trueNeighbors === 3) newBoardStatus[r][c] = true;
+        } else {
+          if (trueNeighbors < 2 || trueNeighbors > 3)
+            newBoardStatus[r][c] = false;
+        }
+      }
+    }
+    // }, 1000);
+    return newBoardStatus;
+  };
+  useEffect(() => {
+    if (gameStatus) {
+      setInterval(() => {
+        setTr(handleStart);
+      }, 1000);
+    }
+
+    return () => {};
+  }, [gameStatus, boardStatus]);
+
+  const renderRow = (tr = []) => {
     for (let row = 0; row < ROWS; row++) {
       const td = [];
       for (let colom = 0; colom < COLOMS; colom++) {
