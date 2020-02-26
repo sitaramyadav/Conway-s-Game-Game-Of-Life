decab9b002d80ce64992f0834e3a2a4b8347d1cc
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-02-26 11:09:36 +0530

    Refactoring the next step to be more readable code.

--- src/components/nextStep.js
+++ src/components/nextStep.js
@@ -1,43 +1,59 @@
-import { ROWS, COLOMS } from "../constants";
+import { ROWS, COLOMS, RELATIVE_NEGHBOUR } from "../constants";
 
-export const nextStep = boardStatus => {
-  const clonedBoardStatus = JSON.parse(JSON.stringify(boardStatus));
-  const amountTrueNeighbors = (r, c) => {
-    const neighbors = [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, 1],
-      [1, 1],
-      [1, 0],
-      [1, -1],
-      [0, -1]
-    ];
-    return neighbors.reduce((trueNeighbors, neighbor) => {
-      const x = r + neighbor[0];
-      const y = c + neighbor[1];
-      const isNeighborOnBoard = x >= 0 && x < ROWS && y >= 0 && y < COLOMS;
-      /* No need to count more than 4 alive neighbors due to rules */
-      if (trueNeighbors < 4 && isNeighborOnBoard && boardStatus[x][y]) {
-        return trueNeighbors + 1;
-      } else {
-        return trueNeighbors;
-      }
-    }, 0);
-  };
+export const nextStep = presentGeneration => {
+  const futureGeneration = JSON.parse(JSON.stringify(presentGeneration));
 
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLOMS; c++) {
-      const totalTrueNeighbors = amountTrueNeighbors(r, c);
+      const totalLiveNeighbors = countLiveNeighbours(presentGeneration, r, c);
 
-      if (!boardStatus[r][c]) {
-        if (totalTrueNeighbors === 3) clonedBoardStatus[r][c] = true;
+      if (isDeadCell(presentGeneration, r, c)) {
+        if (totalLiveNeighbors === 3) futureGeneration[r][c] = true;
       } else {
-        if (totalTrueNeighbors < 2 || totalTrueNeighbors > 3)
-          clonedBoardStatus[r][c] = false;
+        if (totalLiveNeighbors < 2 || totalLiveNeighbors > 3)
+          futureGeneration[r][c] = false;
       }
     }
   }
-
-  return clonedBoardStatus;
+  return futureGeneration;
 };
+function countLiveNeighbours(presentGeneration, r, c) {
+  return RELATIVE_NEGHBOUR.reduce((countOfLiveNeighbours, relativeNeghbour) => {
+    const x = r + relativeNeghbour[0];
+    const y = c + relativeNeghbour[1];
+    const isNeighborOnBoard = isPointOnTheGrid(x, y);
+    /* No need to count more than 4 alive neighbors due to rules */
+    if (
+      canIcreamentLiveNeighbours(countOfLiveNeighbours, isNeighborOnBoard, x, y)
+    ) {
+      return countOfLiveNeighbours + 1;
+    } else {
+      return countOfLiveNeighbours;
+    }
+  }, 0);
+
+  function canIcreamentLiveNeighbours(
+    countOfLiveNeighbours,
+    isNeighborOnBoard,
+    x,
+    y
+  ) {
+    return (
+      countOfLiveNeighbours < 4 &&
+      isNeighborOnBoard &&
+      isLiveCell(presentGeneration, x, y)
+    );
+  }
+}
+
+function isLiveCell(boardStatus, x, y) {
+  return boardStatus[x][y];
+}
+
+function isDeadCell(boardStatus, r, c) {
+  return !isLiveCell(boardStatus, r, c);
+}
+
+function isPointOnTheGrid(x, y) {
+  return x >= 0 && x < ROWS && y >= 0 && y < COLOMS;
+}
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
 export const ROWS = 40;
 export const COLOMS = 60;
+export const RELATIVE_NEGHBOUR = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1]
+];
