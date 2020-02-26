d02022a96c6ea6e1a77e1ca276e36cc5068d5650
Seetaram Yadav <in-seetaram.yadav@Seetarams-MacBook-Pro-2.local>
2020-02-26 13:00:40 +0530

    Extracting the logics for next generation in the small readable functions.

--- src/components/nextStep.js
+++ src/components/nextStep.js
@@ -5,18 +5,41 @@ export const nextStep = presentGeneration => {
 
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLOMS; c++) {
-      const totalLiveNeighbors = countLiveNeighbours(presentGeneration, r, c);
+      const countOfLiveNeighbours = countLiveNeighbours(
+        presentGeneration,
+        r,
+        c
+      );
 
       if (isDeadCell(presentGeneration, r, c)) {
-        if (totalLiveNeighbors === 3) futureGeneration[r][c] = true;
+        if (canBornInTheNextGeneration(countOfLiveNeighbours))
+          futureGeneration[r][c] = true; // true mean it can born
       } else {
-        if (totalLiveNeighbors < 2 || totalLiveNeighbors > 3)
-          futureGeneration[r][c] = false;
+        if (canDieInTheNextGeneration(countOfLiveNeighbours))
+          futureGeneration[r][c] = false; // false means it can die
       }
     }
   }
   return futureGeneration;
 };
+function canBornInTheNextGeneration(countOfLiveNeighbours) {
+  return countOfLiveNeighbours === 3;
+}
+
+function canDieInTheNextGeneration(countOfLiveNeighbours) {
+  return (
+    isLoneliness(countOfLiveNeighbours) || isCrowded(countOfLiveNeighbours)
+  );
+}
+
+function isCrowded(totalLiveNeighbors) {
+  return totalLiveNeighbors > 3;
+}
+
+function isLoneliness(totalLiveNeighbors) {
+  return totalLiveNeighbors < 2;
+}
+
 function countLiveNeighbours(presentGeneration, r, c) {
   return RELATIVE_NEGHBOUR.reduce((countOfLiveNeighbours, relativeNeghbour) => {
     const x = r + relativeNeghbour[0];
